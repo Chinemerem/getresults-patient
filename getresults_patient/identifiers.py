b600ae89cf2295873610015d449d609a028ec2f1
erik van widenfelt <ew2789@gmail.com>
2015-08-16 20:58:19 +0200

    remove ref to identifier history

--- getresults_patient/identifiers.py
+++ getresults_patient/identifiers.py
@@ -1,7 +1,5 @@
 from getresults_identifier import AlphanumericIdentifier
 
-from getresults.models import IdentifierHistory
-
 
 class PatientIdentifier(AlphanumericIdentifier):
 
@@ -9,4 +7,3 @@ class PatientIdentifier(AlphanumericIdentifier):
     alpha_pattern = r'^[A-Z]{3}$'
     numeric_pattern = r'^[0-9]{8}$'
     seed = ['AAA', '00000000']
-    history_model = IdentifierHistory

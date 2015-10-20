977d66fac23b3e8b7756f4d9f07b4338b8fd759e
erikvw <ew2789@gmail.com>
2015-10-20 15:20:15 +0200

    add factory

--- getresults_patient/tests/factories/patient_factory.py
+++ getresults_patient/tests/factories/patient_factory.py
@@ -4,6 +4,7 @@ import factory
 from datetime import datetime
 
 from edc_constants.choices import GENDER
+from django.utils import timezone
 
 
 class PatientFactory(factory.DjangoModelFactory):
@@ -12,7 +13,7 @@ class PatientFactory(factory.DjangoModelFactory):
 
     patient_identifier = factory.Sequence(lambda n: '066-21444678-{0}'.format(n))
     protocol = 'LIS'
-    registration_datetime = datetime.today()
+    registration_datetime = timezone.now()
     gender = GENDER[0]
     dob = datetime(1990, 8, 13)
     identity = factory.Sequence(lambda n: '31791851{0}'.format(n))
